e921a191abe18c1e48b276b44adf3ee96e50610a
Hiro <laciferin@gmail.com>
2021-08-10 05:11:49 +0530

    util/database..js

---
+++ util/database..js
@@ -0,0 +1,9 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize("sequelize", "root", "1234", {
+    dialect: "mysql",
+    host: localhost //is by default
+})
+
+
+module.exports = sequelize
